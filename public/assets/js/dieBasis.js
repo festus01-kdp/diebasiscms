649be0c389ad472492c5c0e8d8cde2c2d79b13ff
Klaus-Dieter Pernak <klaus-dieter.pernak@gmx.de>
2022-02-07 16:19:38 +0100

    basis structure complete with index

---
+++ public/assets/js/dieBasis.js
@@ -0,0 +1,13 @@
+// magnific PopUp
+$('#open-popup').magnificPopup({
+    preloader: false,
+    items: [
+        {
+            //src: 'https://www.diebasis-diepholz-nienburg.de/assets/video/wahlwerbespot-bundestag-2021.mp4',
+            src: 'assets/video/wahlwerbespot-bundestag-2021.mp4?autoplay=1',
+            type: 'iframe'
+            
+            
+        }
+    ]
+});
