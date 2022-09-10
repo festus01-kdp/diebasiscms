2cf75e9875eac8de0c7136d401846e365e50c14a
Klaus-Dieter Pernak <klaus-dieter.pernak@gmx.de>
2022-09-10 17:25:55 +0200

    added Repository for Entity Counter

--- src/Repository/CounterRepository.php
+++ src/Repository/CounterRepository.php
@@ -1,8 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
-class CounterRepository
+use App\Entity\Counter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class CounterRepository extends ServiceEntityRepository
 {
+
+    public function __construct(ManagerRegistry $registry)
+    {
+
+        $manager = $registry->getManager('customer');
+        parent::__construct($registry, Counter::class);
+/*
+ * Fix from https://github.com/symfony/symfony-docs/issues/9878
+ */
+        EntityRepository::__construct($manager, $manager->getClassMetadata(Counter::class));
+    }
 
 }
