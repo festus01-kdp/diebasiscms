308c89a39ced3898be944337b76c4c452cc9d5f3
Klaus-Dieter Pernak <klaus-dieter.pernak@gmx.de>
2022-09-14 17:08:35 +0200

    new Repository

--- src/Repository/CounterRepository.php
+++ src/Repository/CounterRepository.php
@@ -23,4 +23,56 @@ class CounterRepository extends ServiceEntityRepository
         EntityRepository::__construct($manager, $manager->getClassMetadata(Counter::class));
     }
 
+    private function getSumZaehler()
+    {
+
+        $query = $this->_em->createQuery(
+            "SELECT SUM(c.zaehler) summe from App\Entity\Counter c"
+            ." WHERE c.id != 1"
+        );
+
+        return $query->getResult()[0]['summe'];
+
+    }
+//
+    private function getCounterGroupBy()
+    {
+        $query = $this->_em->createQuery(
+            "SELECT c.page, SUM(c.zaehler) summe FROM App\Entity\Counter c"
+            ." WHERE c.id != 1"
+            ."GROUP BY c.page"
+        );
+
+        return $query->getResult();
+    }
+//
+    public function getDataFromCounter():array
+    {
+
+        $returnArray = [];
+        $pages = $this->getCounterGroupBy();
+
+        $sumAccess = $this->getSumZaehler();
+
+        $query = $this->findBy([],['datetime' => 'ASC'],[1]);
+        $firstAccess = $query[0]->getDatetime()->getTimestamp();
+
+        $query = $this->findBy([],['datetime' => 'DESC'],[1]);
+        $lastAccess = $query[0]->getDatetime()->getTimestamp();
+
+
+
+        $returnArray = [
+            'sumAccess' => $sumAccess,
+            'lastAccess' => date('d.m.Y', $lastAccess),
+            'firstAccess' => date('d.m.Y', $firstAccess),
+            'pages' => $pages,
+            ];
+//        Zusätzlichen Eintrag einfügen
+//        $returnArray['V2'] = 2;
+
+        return $returnArray;
+
+    }
+
 }
