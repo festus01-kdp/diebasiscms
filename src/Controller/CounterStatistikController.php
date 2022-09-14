885877ca6fd668e990c8a0795c6ceb3d8a6ec019
Klaus-Dieter Pernak <klaus-dieter.pernak@gmx.de>
2022-09-14 17:07:36 +0200

    new Controller

--- src/Controller/CounterStatistikController.php
+++ src/Controller/CounterStatistikController.php
@@ -2,13 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Counter;
+
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 class CounterStatistikController extends AbstractController
 {
-    public function showCounterStatistik(Request $request)
+
+    protected $doctrine;
+
+    /**
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
     {
-        return $this->render('pages/counterstatistk.html.twig');
+        $this->doctrine = $registry;
+    }
+
+    public function showCounterStatistik()
+    {
+        $data = $this->doctrine->getManager('customer')->getRepository(Counter::class)->getDataFromCounter();
+
+        return $this->render('pages/counterstatistik.html.twig', [
+            'content' => ['counter' => $data],
+        ]);
     }
 }
